b61f1ad279227a338c8bed3e0813840247a7259e
EstherMarie <24815585+EstherMarie@users.noreply.github.com>
2020-06-26 23:17:55 -0300

    variaveis.js e load.js

---
+++ scripts/load.js
@@ -0,0 +1,17 @@
+function preload() {
+    bg_clouds = loadImage('assets/images/background/large-bg-clouds.png');
+    bg_mountains = loadImage('assets/images/background/large-bg-mountains.png');
+    bg_trees = loadImage('assets/images/background/large-bg-trees.png');
+    bg_ground = loadImage('assets/images/background/ground-large.png');
+    sprite = loadImage('assets/images/player/sprites.png');
+    inimigo = loadImage('assets/images/enemies/enemies.png');
+    // gamestart = loadImage('assets/images/sprites/title-screen.png');
+    // pressEnter = loadImage('assets/images/sprites/press-enter.png');
+    gameOver = loadImage('assets/images/sprites/game-over-text.png');
+    music = loadSound('assets/sounds/the_valley.ogg');
+    jumpSound = loadSound('assets/sounds/jump.ogg');
+    itemSound = loadSound('assets/sounds/item.ogg');
+    hurtSound = loadSound('assets/sounds/hurt.ogg');
+    font = loadFont('assets/font/04B_03__.TTF');
+    
+  }
