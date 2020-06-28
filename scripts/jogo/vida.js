477320bda6bcd4b20d47b2e36793c8997cefb8ca
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2020-06-28 13:34:20 -0300

    Update vida.js

--- scripts/jogo/vida.js
+++ scripts/jogo/vida.js
@@ -2,10 +2,10 @@ class Vida {
    constructor(total, inicial) {
    this.total = total;
    this.inicial = inicial;
-
-   this.largura = 50
-   this.altura = 50
    this.vidas = this.inicial
+   this.largura = 25
+   this.altura = 25
+   
    this.xInicial = 20
    this.y = 20
    this.distancia = 0
@@ -15,8 +15,22 @@ class Vida {
 
    draw() {
       for( let i = 0; i < this.vidas; i++) {
+         const margem = i * 10
+         const posicao = this.xInicial * (i + 1)
 
+         image(vida, posicao + margem, this.y, this.largura, this.altura, 25, 25)
       }
+      
+   }
+
+   ganhaVida() {
+      if(this.vidas <= this.total) {
+         this.vidas++
+      }
+   }
+
+   perdeVida() {
+      this.vida--
    }
 
 }
