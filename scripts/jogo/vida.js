589162188c9736158d20253a157a08e03c049615
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2020-06-28 09:19:32 -0300

    Create vida.js

---
+++ scripts/jogo/vida.js
@@ -0,0 +1,22 @@
+class Vida {
+   constructor(total, inicial) {
+   this.total = total;
+   this.inicial = inicial;
+
+   this.largura = 50
+   this.altura = 50
+   this.vidas = this.inicial
+   this.xInicial = 20
+   this.y = 20
+   this.distancia = 0
+   this.novaVidaXInicial = -50
+   this.novaVidaX = this.novaVidaXInicial
+   }
+
+   draw() {
+      for( let i = 0; i < this.vidas; i++) {
+
+      }
+   }
+
+}
